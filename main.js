c9795ef951a540f120451f688192726fb8623a07
Jackson Uptain <111402072+JacksonUptain@users.noreply.github.com>
2023-06-22 11:12:06 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+
 
 import { OrbitControls } from 'https://unpkg.com/three@0.149.0/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'https://unpkg.com/three@0.149.0/examples/jsm/loaders/GLTFLoader.js';
